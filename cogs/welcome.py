203cc56c79a24540a0c5b62e142eba28d23f126b
emma sp <emmaspiridakh@gmail.com>
2026-07-26 14:45:09 +0300

    Change welcome container color to blue

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -29,7 +29,7 @@ class Welcome(commands.Cog):
 
         member_count = member.guild.member_count
 
-        container = ui.Container(accent_colour=discord.Colour.blurple())
+        container = ui.Container(accent_colour=discord.Colour.blue())
 
         if config.WELCOME_BANNER_URL:
             container.add_item(ui.MediaGallery(discord.MediaGalleryItem(media=config.WELCOME_BANNER_URL)))
